d9f7cad8f6cb56e8913ee0e57a6056cea7e6617f
HoanNguyen <nhoxxu197@gmail.com>
2023-08-20 23:30:01 +0700

    Create Player Animator

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotateSpeed = 1f;
+
+    private bool isWalking;
     // Update is called once per frame
     private void Update()
     {
@@ -30,9 +32,14 @@ public class Player : MonoBehaviour
         }
     
         inputVector = inputVector.normalized;
-        
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        isWalking = moveDir != Vector3.zero;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
